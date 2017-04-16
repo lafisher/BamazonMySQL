b09bae140e14ecd22612d96f17b9993bc863df66
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-04-16 14:58:02 -0500

    it's alivegit add .

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,7 @@ connection.connect();
 connection.query('SELECT item_id, product_name, price FROM Products', function(err, result){
 	if(err) console.log("Whoops! something went wrong.");
 
-	//in theory create a table for  database info IDK about this, found this idea on npm + some google fu
+//create table	
 	var table = new Table({
 		head: ['Item Id#', 'Product Name', 'Price'],
 		style: {
@@ -35,87 +35,81 @@ connection.query('SELECT item_id, product_name, price FROM Products', function(e
 	}
 	console.log(table.toString());
 
-	purchase();
+	buyGeek();
 });
 
-//allows user to purchase item from list. or it would if it worked
-var purchase = function(){
+//Prompts customer to buy Geek 
+    var buyGeek = function() {
+        inquirer.prompt([{
+            name: "Item",
+            type: "input",
+            message: "Enter the item id for the geeky item you wish to purchase",
+            validate: function(value) {
 
-	//create prompt
-	var productInfo = {
-		properties: {
-			item_id:{description: ("Enter the item id for the geeky item you wish to purchase")},
-			stock_quantity:{description: ("How many would you like? don't say 42, everyone thinks they're so clever")}
-		},
-	};
+                //Validate answer
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\nPlease enter only the item ID of the item you'd like to buy\n");
+                    return false;
+                }
+            }
+        },
 
-	prompt.start();
+            //customer enter quantity 
+            {
+            name: "Qty",
+            type: "input",
+            message: "How many would you like? don't say 42, everyone thinks they're so clever",
+            validate: function(value) {
+                //validates answer
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\nsomething went wrong, try again!\n");
+                    return false;
+                }
+            }
+        }]).then(function(answer) {
+            var geekPurchase = parseInt(answer.Qty);
 
-/*inquirer.prompt([
-    Pass your questions in here 
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your name?'
-    }
+            //send query to database
+            connection.query("SELECT * FROM Products WHERE ?", [{item_id: answer.Item}], function(err, data) {
+                if (err) throw err;
 
-]).then(function(answer) {
-    //console.log(answer);
-    console.log(answer.username);
-    // Use the answers  for... whatever here!! 
-});*/
+                //Check if quantity exists
+                if (data[0].stock_quantity < geekPurchase) {
+                    console.log("some geek beat you to it! no joy.\n");
+                    console.log("Choose another geek product\n");
+                    start();
+                } else {
 
-	//get responses from prompt 
-	prompt.get(productInfo, function(err, res){
+                    //If quantity matches update database
+                    var updateQty = data[0].stock_quantity - geekPurchase;
+                    var totalPrice = data[0].price * geekPurchase;
+                    connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [updateQty, answer.Item], function(err, results) {
+                        if(err) {
+                            throw err;
+                        } else {
+                            console.log("Geek purchase complete!\n");
+                            console.log("Your total cost is: $ " + totalPrice);
 
-		//create geekPurcahse var
-		var geekPurchase = {
-			item_id: res.item_id,
-			quantity: res.quantity
-		};
-	
-		//push to productPurchased
-		productPurchased.push(geekPurchase);
-
-		//connect to database 
-		connection.query('SELECT * FROM Products WHERE item_id=?', productPurchased[0].item_id, function(err, res){
-				if(err) console.log(err, "dang, i could have sworn you got game.");
-				
-				//if purchase order is greater than stock 
-				if(res[0].stock_quantity < productPurchased[0].quantity){
-					console.log("some geek beat you to it! no joy.");
-					connection.end();
-
-				//if there is enough stock 
-				} else if(res[0].stock_quantity >= productPurchased[0].quantity){
-
-					console.log('');
-
-					console.log(productPurchased[0].quantity + ' items purchased');
-
-					console.log(res[0].product_name + ' ' + res[0].price);
-
-					//var for purchase total 
-					var saleTotal = res[0].price * productPurchased[0].quantity;
-						console.log('Total: ' + saleTotal);
-
-					//this variable contains the newly updated stock quantity of the item purchased
-					newQuantity = res[0].stock_quantity - productPurchased[0].quantity;
-			
-					// connects to the mysql database products and updates the stock quantity for the item puchased
-					connection.query("UPDATE Products SET stock_quantity = " + newQuantity +" WHERE item_id = " + productPurchased[0].item_id, function(err, res){
-						if(err) throw err;
-						console.log('Problem ', err);
-						console.log('');
-						console.log("Processing.  Thank you for shopping Bamazon-Geek!");
-						console.log('');
-
-						connection.end();
-					})
-
-				};
-
-		})
-	})
-
-};
+                            //Ask buyer to continue
+                            inquirer.prompt({
+                                name: "buyMore",
+                                type: "confirm",
+                                message: "Would you like to buy more geek products?",
+                            }).then(function(answer) {
+                                if (answer.buyMore === true) {
+                                    start();
+                                } else {
+                                    console.log("Processing.  Thank you for shopping Bamazon-Geek!");
+                                    connection.end();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    };
